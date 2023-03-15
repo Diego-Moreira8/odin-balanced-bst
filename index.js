3b62a7cfadaab4890167c5500420ad2207cd88b3
Diego Moreira <diego.moreira529@gmail.com>
2023-03-15 20:59:48 -0300

    Finished buildTree method and added prettyPrint

--- index.js
+++ index.js
@@ -11,36 +11,46 @@ class Node {
 class Tree {
   constructor(_array) {
     this.array = _array;
-    this.root;
+    this.root = this.buildTree(_array);
   }
 
   buildTree(array) {
-    // For the first call
-    if (array === undefined) {
-      array = this.array;
-    }
-
-    // >>> Maybe a condition for length===2
     if (array.length === 1) {
-      return new Node("DATA");
+      return new Node(array[0]);
+    } else if (array.length === 2) {
+      return new Node(array[0], this.buildTree(array.slice(1)));
+    } else {
+      // Finds the INDEX of the middle element
+      const midIndex = Math.floor(array.length / 2);
+
+      // Slices the array
+      const leftArray = array.slice(0, midIndex);
+      const rightArray = array.slice(midIndex + 1);
+
+      // Recursively creates new nodes
+      const root = new Node(
+        array[midIndex],
+        this.buildTree(leftArray),
+        this.buildTree(rightArray)
+      );
+
+      // Returns the generated node
+      return root;
     }
+  }
 
-    // Finds the INDEX of the middle element
-    const midIndex = Math.floor(array.length / 2);
-
-    // Slices the array
-    const leftArray = array.slice(0, midIndex);
-    const rightArray = array.slice(midIndex + 1);
-
-    // Recursively creates new nodes
-    const root = new Node(
-      "DATA",
-      this.buildTree(leftArray),
-      this.buildTree(rightArray)
-    );
-
-    // Returns the generated node
-    return root;
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }
 
@@ -49,4 +59,4 @@ const sortedArray = mergeSort([
   1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
 ]);
 const tree = new Tree(sortedArray);
-tree.buildTree();
+console.log(tree.prettyPrint());
