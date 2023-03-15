96928178b97205bc7015e01b29a567e769d93e83
Diego Moreira <diego.moreira529@gmail.com>
2023-03-14 21:47:57 -0300

    Creating buildTree method

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ class Node {
 class Tree {
   constructor(_array) {
     this.array = _array;
+    this.root;
   }
 
-  buildTree() {
-    console.log(this.array);
+  buildTree(array) {
+    // For the first call
+    if (array === undefined) {
+      array = this.array;
+    }
+
+    // >>> Maybe a condition for length===2
+    if (array.length === 1) {
+      return new Node("DATA");
+    }
+
+    // Finds the INDEX of the middle element
+    const midIndex = Math.floor(array.length / 2);
+
+    // Slices the array
+    const leftArray = array.slice(0, midIndex);
+    const rightArray = array.slice(midIndex + 1);
+
+    // Recursively creates new nodes
+    const root = new Node(
+      "DATA",
+      this.buildTree(leftArray),
+      this.buildTree(rightArray)
+    );
+
+    // Returns the generated node
+    return root;
   }
 }
 
@@ -23,3 +49,4 @@ const sortedArray = mergeSort([
   1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
 ]);
 const tree = new Tree(sortedArray);
+tree.buildTree();
